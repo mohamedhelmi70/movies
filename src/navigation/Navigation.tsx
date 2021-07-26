5abffbfbbf4d4e3bfce6871f1f72ce1fe1dcf857
Mohamed Helmi <mtabila37@gmail.com>
2021-07-27 01:56:26 +0200

    add title for movies header

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -3,15 +3,49 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {createStackNavigator} from '@react-navigation/stack';
 import {RootParamList} from '../../types';
 import {Movies, Movie} from '../screens';
+import {Text, View} from '../components/Themed';
+import {wp} from '../constants/Layout';
+import Colors from '../constants/Colors';
 
 const Stack = createStackNavigator<RootParamList>();
 
 function Navigation() {
     return (
-        <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}} edges={['bottom', 'left', 'right']}>
+        <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}} edges={['left', 'right']}>
             <Stack.Navigator initialRouteName="Movies" screenOptions={{headerTitleAlign: 'center'}}>
-                <Stack.Screen name="Movies" component={Movies} />
-                <Stack.Screen name="Movie" component={Movie} />
+                <Stack.Screen
+                    name="Movies"
+                    component={Movies}
+                    options={{
+                        headerStyle: {
+                            shadowColor: Colors.light.background,
+                        },
+                        headerTitle: '',
+                        headerRight: () => <View />,
+                        headerLeft: () => (
+                            <View>
+                                <Text
+                                    style={{fontSize: wp('6%'), marginHorizontal: wp('5%')}}
+                                    weight="700"
+                                    font="Roboto-Bold">
+                                    Movies
+                                </Text>
+                            </View>
+                        ),
+                    }}
+                />
+                <Stack.Screen
+                    name="Movie"
+                    component={Movie}
+                    options={{
+                        headerStyle: {
+                            shadowColor: Colors.light.background,
+                        },
+                        headerTitle: '',
+                        headerRight: () => <View />,
+                        headerLeft: () => <View />,
+                    }}
+                />
             </Stack.Navigator>
         </SafeAreaView>
     );
