7852c6c587cbaa308401055685e44cc813026345
Mohamed Helmi <mtabila37@gmail.com>
2021-07-27 09:32:20 +0200

    finish movies page

--- src/screens/Movies/Movies.tsx
+++ src/screens/Movies/Movies.tsx
@@ -1,20 +1,137 @@
+import axios from 'axios';
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
-import {Text} from '../../components/Themed';
+import {FlatList, Pressable, StyleSheet} from 'react-native';
+import {Genre, Movie} from '../../../types';
+import {Container} from '../../components';
+import {Text, View} from '../../components/Themed';
+import Colors from '../../constants/Colors';
+import {wp} from '../../constants/Layout';
+import {useStore} from '../../contexts/StoreContext';
+import useIsMountedRef from '../../hooks/useIsMountedRef';
+import MovieCard from './MovieCard';
 
 function Movies() {
+    const isMountedRef = useIsMountedRef();
+    const [tab, setTab] = React.useState<'upcoming' | 'top_rated' | 'popular'>('upcoming');
+    const [page, setPage] = React.useState<number>(1);
+    const [movies, setMovies] = React.useState<Movie[]>([]);
+    const {genres} = useStore();
+
+    const getMovies = React.useCallback(async () => {
+        try {
+            const response = await axios.get(
+                `https://api.themoviedb.org/3/movie/${tab}?api_key=4f298a53e552283bee957836a529baec&language=en-US&page=${page}`,
+            );
+            const moviesWithGenres: Movie[] = [];
+            if (response?.data?.results?.length > 0) {
+                for (const movie of response?.data?.results) {
+                    const movieWithGenres: Movie = {
+                        ...movie,
+                        genres: genres?.filter((genre: Genre) => movie?.genre_ids?.includes(genre?.id)),
+                    };
+                    moviesWithGenres.push(movieWithGenres);
+                }
+                if (isMountedRef?.current) {
+                    setMovies(moviesWithGenres);
+                }
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }, [isMountedRef, tab, page, genres]);
+
+    React.useEffect(() => {
+        getMovies();
+    }, [getMovies]);
+
     return (
-        <View style={styles.container}>
-            <Text>Movies</Text>
-        </View>
+        <Container style={styles.container}>
+            <View style={styles.tabs}>
+                <Pressable
+                    onPress={tab !== 'upcoming' ? () => setTab('upcoming') : undefined}
+                    style={[styles.tab, tab === 'upcoming' ? styles.activeTab : {}]}>
+                    <Text
+                        lightColor={tab === 'upcoming' ? '#FFF' : '#000'}
+                        style={styles.tabText}
+                        weight="700"
+                        font="Roboto-Medium">
+                        Upcoming
+                    </Text>
+                </Pressable>
+                <Pressable
+                    onPress={tab !== 'popular' ? () => setTab('popular') : undefined}
+                    style={[styles.tab, tab === 'popular' ? styles.activeTab : {}]}>
+                    <Text
+                        lightColor={tab === 'popular' ? '#FFF' : '#000'}
+                        style={styles.tabText}
+                        weight="700"
+                        font="Roboto-Medium">
+                        Popular
+                    </Text>
+                </Pressable>
+                <Pressable
+                    onPress={tab !== 'top_rated' ? () => setTab('top_rated') : undefined}
+                    style={[styles.tab, tab === 'top_rated' ? styles.activeTab : {}]}>
+                    <Text
+                        lightColor={tab === 'top_rated' ? '#FFF' : '#000'}
+                        style={styles.tabText}
+                        weight="700"
+                        font="Roboto-Medium">
+                        Top Rated
+                    </Text>
+                </Pressable>
+            </View>
+            <FlatList
+                style={{flex: 1, width: '100%'}}
+                data={movies}
+                contentContainerStyle={{justifyContent: 'flex-start', paddingVertical: wp('5%')}}
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
+                keyExtractor={(item) => item?.id.toString()}
+                onEndReachedThreshold={0.2}
+                renderItem={({item}) => <MovieCard movie={item} onPress={() => {}} />}
+            />
+        </Container>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
+    },
+    tabs: {
+        marginTop: wp('2%'),
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: wp('100%'),
+        paddingHorizontal: wp('5%'),
+        paddingBottom: wp('2%'),
+    },
+    tab: {
+        width: wp('28%'),
+        borderRadius: wp('5%'),
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ddd',
+        paddingVertical: wp('2.5%'),
+    },
+    activeTab: {
+        backgroundColor: Colors.light.primary,
+
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    tabText: {
+        fontSize: wp('4%'),
     },
 });
 
