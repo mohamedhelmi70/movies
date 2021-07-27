2acfc4556d539a4f3032a1d46807179eb8c7af1c
Mohamed Helmi <mtabila37@gmail.com>
2021-07-27 04:56:57 +0200

    add Genre interface add movie interface

--- types.ts
+++ types.ts
@@ -4,3 +4,25 @@ export type RootParamList = {
         movieId: number;
     };
 };
+
+export interface Movie {
+    poster_path: string;
+    adult: boolean;
+    overview: string;
+    release_date: string,
+    genre_ids: number[],
+    id: number,
+    original_title: string,
+    original_language: string,
+    title: string,
+    backdrop_path: string,
+    popularity: number,
+    vote_count: number,
+    video: boolean,
+    vote_average: number
+}
+
+export interface Genre {
+    id: number;
+    name: string;
+}
